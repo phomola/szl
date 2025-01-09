15615f01ce855ac67578482de3754dbefc064cc8
Petr Homola <phomola@me.com>
2025-01-09 09:11:51 +0000

    Morph (#1)
    * morph
    
    * windows binary
    
    * fixed typo
    
    * -list command line parameter
    
    * chart generation
    
    * Silesian doc (#2)
    
    * Create README.md
    
    * Add files via upload
    
    * Add files via upload
    
    * updated szl doc
    
    * replacer
    
    * simplified silesian lex
    
    * Chart parser (#3)
    
    * chart parser
    
    * parsing AVMs
    
    * parsing
    
    * restoration of full orthography
    
    * subst upper/lower case
    
    * Lexicon (#4)
    
    * paradigms comments
    
    * paradigms
    
    * adjectives
    
    * neut subst
    
    * Verbs (#5)
    
    * v4 pres
    
    * result as paths
    
    * paths of clusters as result
    
    * subj/obj rules
    
    * checking valence
    
    * valence update
    
    * v5 & v6
    
    * v4
    
    * v4 dz
    
    * v3
    
    * v2
    
    * v2 - 2 subtypes
    
    * v1a
    
    * token extractor (#6)
    
    * word count package
    
    * godoc (#7)
    
    * v1b
    
    * v1c & v1d

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/phomola/szl
+
+go 1.23.0
+
+require (
+	github.com/fealsamh/go-utils v0.1.36
+	github.com/phomola/lrparser v0.1.8
+	github.com/phomola/textkit v0.1.5
+)
---
+++ syntax/edge.go
@@ -0,0 +1,37 @@
+package syntax
+
+import (
+	"fmt"
+	"io"
+)
+
+// Edge is a chart edge.
+type Edge struct {
+	Start    int
+	End      int
+	Category string
+	Form     string
+	AVM      *AVM
+	Info     map[string]interface{}
+	Children []*Edge
+	Level    int
+	Used     bool
+	Weight   int
+}
+
+// Print prints the edge.
+func (e *Edge) Print(w io.Writer) {
+	fmt.Fprintf(w, "-%d- %s %s %s -%d-\n", e.Start, e.Form, e.Category, e.AVM.String(), e.End)
+}
+
+// Linearise linearises the syntax tree.
+func (e *Edge) Linearise(f func(*Edge) string) []string {
+	if e.Children == nil {
+		return []string{f(e)}
+	}
+	var l []string
+	for _, e := range e.Children {
+		l = append(l, e.Linearise(f)...)
+	}
+	return l
+}
